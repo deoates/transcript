5e3f717aa24261edf4c9849eebdd3a9635fabbf9
David Oates <deoates@gmail.com>
2015-01-15 16:33:05 -0500

    change how URLs are handled

--- coffee/scripts.coffee
+++ coffee/scripts.coffee
@@ -1,7 +1,10 @@
 dropzoneTemplate = """
   <div class="dz-preview dz-file-preview">
     <div class="dz-details">
-      <div class="dz-filename"><span data-dz-name></span></div>
+      <div class="dz-filename">
+        <i class="fa fa-file-audio-o"></i>
+        <span data-dz-name></span>
+      </div>
       <div class="dz-size" data-dz-size></div>
     </div>
     <div class="dz-progress-container">
@@ -34,12 +37,19 @@ $ ->
     mixpanel.track "Clicked to add URLs"
 
 
+
+  urls = []
+
   dropzone = new Dropzone 'a[data-href="dropzone"]',
     paramName: 'asset'
     previewsContainer: '.dz-preview-container'
     previewTemplate: dropzoneTemplate
     createImageThumbnails: false
     url: "#{api}/upload"
+
+  $('a[data-href="dropzone"]').click (e) ->
+    do e.preventDefault
+    $('a[data-href="dropzone"]').trigger 'click'
 
   dropzone.on 'processing', () ->
     $('.helper').fadeOut()
@@ -59,49 +69,75 @@ $ ->
   dropzone.on 'success', (file) ->
     $('.dz-status').text "Upload complete!"
     $('a[data-href="cancel"]').text "Delete file"
-    $('a[data-href="save-files"]').fadeIn()
     mixpanel.track "File upload successful"
+    do checkFiles
 
-
-  # handle adding URLS
   $('a[data-href="saveURL"]').click (e) ->
     do e.preventDefault
-    urls = $('textarea#URLs').val()
-
-    $('.helper').hide()
+    unless $('input#URL').val() is ''
+      urls.push $('input#URL').val()
     $('#urlModal').foundation('reveal', 'close')
-    $('.added-URLs').html urls
-    $('.added-URLs-container').show()
-    $('a[data-href="save-files"]').fadeIn()
-    mixpanel.track "Added URLs"
+    do refreshURLs
 
-  $('a[data-href="delete-URLs"]').click (e) ->
-    do e.preventDefault
-    $('.added-URLs').html ''
-    $('.added-URLs-container').hide()
-    if dropzone.files.length is 0
-      $('.helper').fadeIn()
+  refreshURLs = () ->
+    $('ul.added-URLs').html ''
+
+    for url, index in urls
+      $('ul.added-URLs').append """
+        <li data-URL-index='#{index}'>
+          <span><i class="fa fa-link"></i> #{url}</span>
+          <div class="dz-actions">
+            <a class="warn" data-href="delete-URL" href="#">Remove</a>
+          </div>
+        </li>
+      """
+
+    $('ul.added-URLs').show()
+    do checkFiles
+    do deleteHandler
+
+  deleteHandler = ->
+    $('a[data-href="delete-URL"]').click (e) ->
+      do e.preventDefault
+      i = $(e.currentTarget).closest('li').attr('data-URL-index')
+      urls.splice(i, 1)
+      do refreshURLs
+
+  checkFiles = ->
+    if urls.length > 0 or dropzone.files.length > 0
+      $('.helper').hide()
+      $('a[data-href="save-files"]').show()
+    else 
+      $('.helper').show()
       $('a[data-href="save-files"]').hide()
 
+
+  $('a[data-href="back"]').click (e) ->
+    $('a[href="#panel1"]').trigger('click')
+    
+
   $('a[data-href="save-files"]').click (e) ->
-    urls = $('.addedURLs').text()
+
     files = []
 
-    if (dropzone.files.length is 0 && urls is "")
-      return
-
     for file in dropzone.files
-      files.push file.name
+      if file.status = 'success'
+        files.push file.name
 
-    unless urls is ""
-      files.push urls
+    return if (files.length is 0 and urls.length is 0)
 
-    window.localStorage['files'] = JSON.stringify(files)
-    console.log window.localStorage['files']
+    fileString = ""
+    
+    for file in files
+      fileString += "File: #{file} \n"
+    for url in urls
+      fileString += "URL: #{url} \n"
+
+    window.localStorage['files'] = fileString
     $('a[href="#panel2"]').trigger('click')
-    mixpanel.track "Clicked to save files"
 
   order = 0.00
+  description = ""
   price = .99
   hours = 0
   mins = 0
@@ -118,7 +154,7 @@ $ ->
       mins = "0" + mins.toString()
 
     $('.order-amount').html "$#{order.toFixed(2)}"
-    $('.order-description').html "#{hours}:#{mins} of audio to be transcribed at $#{price}/minute"
+    $('.order-description').html "#{hours} hours, #{mins} minutes to be transcribed at $#{price}/minute <br> #{window.localStorage['files']}"
 
   $('.hour-count, .min-count').keyup (e) ->
     do updateOrderAmt
@@ -133,22 +169,34 @@ $ ->
 
   handler = {}
 
-  $.ajax "#{api}/stripeKey",
-    method: 'GET'
-    success: (key) =>
-      handler = StripeCheckout.configure
-        key: key
-        image: 'images/stripe-img.png'
-        token: (token) ->
-          # Use the token to create the charge with a server-side script.
-          # You can access the token ID with `token.id`
-          console.log token.id
-          $('a[href="#panel3"]').trigger('click')
-          mixpanel.track "Paid successfully"
+  handlerSetup = ->
+    $.ajax "#{api}/stripeKey",
+      method: 'GET'
+      success: (key) ->
+        handler = StripeCheckout.configure
+          key: key
+          image: 'images/stripe-img.png'
+          token: (token) ->
+            $.ajax "#{api}/order",
+              method: 'POST'
+              data:
+                'token': token.id
+                'email': email
+                'amount': order * 100
+                'files': window.localStorage['files']
+                'length': "#{hours}:#{mins}"
+              success: (response) ->
+                $('a[href="#panel3"]').trigger('click')
+                mixpanel.track "Paid successfully"
+
+  do handlerSetup
 
   $('#checkout').on 'click', (e) ->
     # open Checkout with further options
     mixpanel.track "Clicked to pay"
+
+    unless handler?
+      do handlerSetup
 
     handler.open
       name: 'Audio Transcription'
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 (function() {
   var dropzoneTemplate;
 
-  dropzoneTemplate = "<div class=\"dz-preview dz-file-preview\">\n  <div class=\"dz-details\">\n    <div class=\"dz-filename\"><span data-dz-name></span></div>\n    <div class=\"dz-size\" data-dz-size></div>\n  </div>\n  <div class=\"dz-progress-container\">\n    <div class=\"dz-progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div>\n    <span class=\"dz-progress-percent\"></span>\n  </div>\n  <div class=\"dz-actions\">\n    <span data-dz-errormessage class=\"dz-status\">Uploading file...</span>\n    <a class=\"warn\" data-dz-remove data-href=\"cancel\" href=\"#\">Cancel</a>\n  </div>\n</div>";
+  dropzoneTemplate = "<div class=\"dz-preview dz-file-preview\">\n  <div class=\"dz-details\">\n    <div class=\"dz-filename\">\n      <i class=\"fa fa-file-audio-o\"></i>\n      <span data-dz-name></span>\n    </div>\n    <div class=\"dz-size\" data-dz-size></div>\n  </div>\n  <div class=\"dz-progress-container\">\n    <div class=\"dz-progress\"><span class=\"dz-upload\" data-dz-uploadprogress></span></div>\n    <span class=\"dz-progress-percent\"></span>\n  </div>\n  <div class=\"dz-actions\">\n    <span data-dz-errormessage class=\"dz-status\">Uploading file...</span>\n    <a class=\"warn\" data-dz-remove data-href=\"cancel\" href=\"#\">Cancel</a>\n  </div>\n</div>";
 
   $(function() {
-    var api, dropzone, email, handler, hours, mins, order, price, updateOrderAmt;
+    var api, checkFiles, deleteHandler, description, dropzone, email, handler, handlerSetup, hours, mins, order, price, refreshURLs, updateOrderAmt, urls;
     api = "http://api.transcriptengine.com";
     if (document.location.hostname === 'localhost') {
       api = "http://localhost:5000";
@@ -21,12 +21,17 @@
     $('a[data-reveal-id="urlModal"]').click(function(e) {
       return mixpanel.track("Clicked to add URLs");
     });
+    urls = [];
     dropzone = new Dropzone('a[data-href="dropzone"]', {
       paramName: 'asset',
       previewsContainer: '.dz-preview-container',
       previewTemplate: dropzoneTemplate,
       createImageThumbnails: false,
       url: "" + api + "/upload"
+    });
+    $('a[data-href="dropzone"]').click(function(e) {
+      e.preventDefault();
+      return $('a[data-href="dropzone"]').trigger('click');
     });
     dropzone.on('processing', function() {
       return $('.helper').fadeOut();
@@ -46,50 +51,76 @@
     dropzone.on('success', function(file) {
       $('.dz-status').text("Upload complete!");
       $('a[data-href="cancel"]').text("Delete file");
-      $('a[data-href="save-files"]').fadeIn();
-      return mixpanel.track("File upload successful");
+      mixpanel.track("File upload successful");
+      return checkFiles();
     });
     $('a[data-href="saveURL"]').click(function(e) {
-      var urls;
       e.preventDefault();
-      urls = $('textarea#URLs').val();
-      $('.helper').hide();
+      if ($('input#URL').val() !== '') {
+        urls.push($('input#URL').val());
+      }
       $('#urlModal').foundation('reveal', 'close');
-      $('.added-URLs').html(urls);
-      $('.added-URLs-container').show();
-      $('a[data-href="save-files"]').fadeIn();
-      return mixpanel.track("Added URLs");
+      return refreshURLs();
     });
-    $('a[data-href="delete-URLs"]').click(function(e) {
-      e.preventDefault();
-      $('.added-URLs').html('');
-      $('.added-URLs-container').hide();
-      if (dropzone.files.length === 0) {
-        $('.helper').fadeIn();
+    refreshURLs = function() {
+      var index, url, _i, _len;
+      $('ul.added-URLs').html('');
+      for (index = _i = 0, _len = urls.length; _i < _len; index = ++_i) {
+        url = urls[index];
+        $('ul.added-URLs').append("<li data-URL-index='" + index + "'>\n  <span><i class=\"fa fa-link\"></i> " + url + "</span>\n  <div class=\"dz-actions\">\n    <a class=\"warn\" data-href=\"delete-URL\" href=\"#\">Remove</a>\n  </div>\n</li>");
+      }
+      $('ul.added-URLs').show();
+      checkFiles();
+      return deleteHandler();
+    };
+    deleteHandler = function() {
+      return $('a[data-href="delete-URL"]').click(function(e) {
+        var i;
+        e.preventDefault();
+        i = $(e.currentTarget).closest('li').attr('data-URL-index');
+        urls.splice(i, 1);
+        return refreshURLs();
+      });
+    };
+    checkFiles = function() {
+      if (urls.length > 0 || dropzone.files.length > 0) {
+        $('.helper').hide();
+        return $('a[data-href="save-files"]').show();
+      } else {
+        $('.helper').show();
         return $('a[data-href="save-files"]').hide();
       }
+    };
+    $('a[data-href="back"]').click(function(e) {
+      return $('a[href="#panel1"]').trigger('click');
     });
     $('a[data-href="save-files"]').click(function(e) {
-      var file, files, urls, _i, _len, _ref;
-      urls = $('.addedURLs').text();
+      var file, fileString, files, url, _i, _j, _k, _len, _len1, _len2, _ref;
       files = [];
-      if (dropzone.files.length === 0 && urls === "") {
-        return;
-      }
       _ref = dropzone.files;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         file = _ref[_i];
-        files.push(file.name);
+        if (file.status = 'success') {
+          files.push(file.name);
+        }
       }
-      if (urls !== "") {
-        files.push(urls);
+      if (files.length === 0 && urls.length === 0) {
+        return;
       }
-      window.localStorage['files'] = JSON.stringify(files);
-      console.log(window.localStorage['files']);
-      $('a[href="#panel2"]').trigger('click');
-      return mixpanel.track("Clicked to save files");
+      fileString = "";
+      for (_j = 0, _len1 = files.length; _j < _len1; _j++) {
+        file = files[_j];
+        fileString += "File: " + file + " \n";
+      }
+      for (_k = 0, _len2 = urls.length; _k < _len2; _k++) {
+        url = urls[_k];
+        fileString += "URL: " + url + " \n";
+      }
+      window.localStorage['files'] = fileString;
+      return $('a[href="#panel2"]').trigger('click');
     });
     order = 0.00;
+    description = "";
     price = .99;
     hours = 0;
     mins = 0;
@@ -105,7 +136,7 @@
         mins = "0" + mins.toString();
       }
       $('.order-amount').html("$" + (order.toFixed(2)));
-      return $('.order-description').html("" + hours + ":" + mins + " of audio to be transcribed at $" + price + "/minute");
+      return $('.order-description').html("" + hours + " hours, " + mins + " minutes to be transcribed at $" + price + "/minute <br> " + window.localStorage['files']);
     };
     $('.hour-count, .min-count').keyup(function(e) {
       updateOrderAmt();
@@ -116,24 +147,39 @@
       return mixpanel.track("Entered email address");
     });
     handler = {};
-    $.ajax("" + api + "/stripeKey", {
-      method: 'GET',
-      success: (function(_this) {
-        return function(key) {
+    handlerSetup = function() {
+      return $.ajax("" + api + "/stripeKey", {
+        method: 'GET',
+        success: function(key) {
           return handler = StripeCheckout.configure({
             key: key,
             image: 'images/stripe-img.png',
             token: function(token) {
-              console.log(token.id);
-              $('a[href="#panel3"]').trigger('click');
-              return mixpanel.track("Paid successfully");
+              return $.ajax("" + api + "/order", {
+                method: 'POST',
+                data: {
+                  'token': token.id,
+                  'email': email,
+                  'amount': order * 100,
+                  'files': window.localStorage['files'],
+                  'length': "" + hours + ":" + mins
+                },
+                success: function(response) {
+                  $('a[href="#panel3"]').trigger('click');
+                  return mixpanel.track("Paid successfully");
+                }
+              });
             }
           });
-        };
-      })(this)
-    });
+        }
+      });
+    };
+    handlerSetup();
     $('#checkout').on('click', function(e) {
       mixpanel.track("Clicked to pay");
+      if (handler == null) {
+        handlerSetup();
+      }
       handler.open({
         name: 'Audio Transcription',
         description: "" + hours + " hours, " + mins + " mins - $" + (order.toFixed(2)),
